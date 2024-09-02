f5f5d60b087d71dbfea769d5ab7d2b5a78133435
Akintola Jacob <akintola.jacob@example.com>
2024-09-02 12:59:49 +0100

    Add name generator project

---
+++ projects/Name_generator/main.py
@@ -0,0 +1,7 @@
+# Python script that generates a band name based on user input.
+
+
+print("Welcome to the Name Generator.")
+first_input = input("What's your favorite place in the world?\n").capitalize()
+second_input = input("What name do you like the most?\n").capitalize()
+print(f"Your name could be '{first_input} {second_input}'")
